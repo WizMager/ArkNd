706f6c0b04d2f6aedcb90569fb7b21642e739610
WizMager <wizmager@gmailcom>
2025-11-17 12:52:46 +0300

    -init

---
+++ Assets/Scripts/Bootstrap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Core;
+using Core.Interfaces;
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+    private IModulesHandler _modulesHandler;
+    private void Awake()
+    {
+        List<IModule> modules = new();
+        
+        _modulesHandler = new ModulesHandler(modules);
+        
+        _modulesHandler.Awake();
+    }
+
+    private void Start()
+    {
+        _modulesHandler.Start();
+    }
+    
+    private void FixedUpdate()
+    {
+        _modulesHandler.FixedUpdate();
+    }
+    
+    private void Update()
+    {
+        _modulesHandler.Update();
+    }
+}
