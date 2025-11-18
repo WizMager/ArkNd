5c26830f36a5cd50460067a3c86e68b042a732cc
WizMager <wizmager@gmailcom>
2025-11-18 13:49:40 +0300

    -add check only one collision in frame for ball

--- Assets/Scripts/Views/BallView.cs
+++ Assets/Scripts/Views/BallView.cs
@@ -7,12 +7,19 @@ namespace Views
     {
         public Action<Vector2> OnTouch;
 
+        private int _lastCollisionFrame = -1;
+
         [field:SerializeField] public Rigidbody2D Rigidbody { get; private set; }
         
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.layer != LayerMask.NameToLayer("Reflect"))
                 return;
+
+            if (_lastCollisionFrame == Time.frameCount)
+                return;
+
+            _lastCollisionFrame = Time.frameCount;
             
             OnTouch?.Invoke(other.contacts[0].normal);
         }
